69a937c33963a5b0082e4effdccff919008e446d
VEN1KFC <157003153+Lover379@users.noreply.github.com>
2026-05-25 15:36:31 +0800

    Create main.cpp

---
+++ Homework/Moon/main.cpp
@@ -0,0 +1,119 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <windows.h>
+#include <cstdio>
+#include "datetime.h"
+
+using namespace std;
+
+void print_time(int hms) {
+    int h = hms / 10000;
+    int m = (hms / 100) % 100;
+    int s = hms % 100;
+    if (h < 10) cout << "0"; cout << h << ":";
+    if (m < 10) cout << "0"; cout << m << ":";
+    if (s < 10) cout << "0"; cout << s;
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    int dd, mm, yyyy;
+    char dot;
+    cout << "Введите дату (дд.мм.гггг): ";
+
+    if (!(cin >> dd >> dot >> mm >> dot >> yyyy)) {
+        cout << "Ошибка ввода даты!" << endl;
+        return 1;
+    }
+
+    DateTime targetDate(dd, mm, yyyy);
+    if (!targetDate.checkDate(dd, mm, yyyy)) {
+        cout << "Ошибка: неверная дата" << endl;
+        return 1;
+    }
+
+    char fname[20];
+    sprintf(fname, "moon%04d.dat", yyyy);
+
+    FILE* f = fopen(fname, "r");
+    if (!f) {
+        cout << "Файл " << fname << " не найден" << endl;
+        return 1;
+    }
+
+    char header[256];
+    if (!fgets(header, sizeof(header), f)) {
+        cout << "Ошибка чтения структуры файла." << endl;
+        fclose(f);
+        return 1;
+    }
+
+    int rise = -1, set = -1, culm = -1;
+    double max_el = -99.0;
+    
+    double prev_el = 0.0; 
+    int prev_hms = -1;
+    int targetYMD = yyyy * 10000 + mm * 100 + dd;
+
+    int ymd, hms;
+    double T, R, El, Az, FI, LG;
+    bool dataFound = false;
+
+    while (fscanf(f, "%d %d %lf %lf %lf %lf %lf %lf", &ymd, &hms, &T, &R, &El, &Az, &FI, &LG) == 8) {
+        
+        if (ymd == targetYMD) {
+            dataFound = true;
+
+            if (prev_hms != -1) {
+                if (prev_el < 0 && El >= 0 && rise == -1) {
+                    double ratio = -prev_el / (El - prev_el);
+                    int p = (prev_hms / 10000) * 3600 + ((prev_hms / 100) % 100) * 60 + (prev_hms % 100);
+                    int c = (hms / 10000) * 3600 + ((hms / 100) % 100) * 60 + (hms % 100);
+                    int r = p + (int)(ratio * (c - p) + 0.5);
+                    rise = (r / 3600) * 10000 + ((r % 3600) / 60) * 100 + (r % 60);
+                }
+                if (prev_el >= 0 && El < 0 && set == -1) {
+                    double ratio = prev_el / (prev_el - El);
+                    int p = (prev_hms / 10000) * 3600 + ((prev_hms / 100) % 100) * 60 + (prev_hms % 100);
+                    int c = (hms / 10000) * 3600 + ((hms / 100) % 100) * 60 + (hms % 100);
+                    int s = p + (int)(ratio * (c - p) + 0.5);
+                    set = (s / 3600) * 10000 + ((s % 3600) / 60) * 100 + (s % 60);
+                }
+            }
+
+            if (El > max_el) { 
+                max_el = El; 
+                culm = hms; 
+            }
+
+            prev_el = El; 
+            prev_hms = hms;
+        } 
+        else if (dataFound && ymd > targetYMD) {
+            break;
+        }
+    }
+    fclose(f);
+
+    if (!dataFound) { 
+        cout << "Данные на указанную дату отсутствуют в файле." << endl; 
+        return 1; 
+    }
+
+    cout << "Дата: "; targetDate.showFormat1(); cout << "\n";
+    cout << "День недели: " << targetDate.weekDay() << endl;
+    
+    cout << "Восход: "; 
+    if (rise >= 0) print_time(rise); else cout << "---";
+    
+    cout << "\nКульминация: "; 
+    if (culm >= 0) print_time(culm); else cout << "---";
+    
+    cout << "\nЗаход: "; 
+    if (set >= 0) print_time(set); else cout << "---";
+    cout << endl;
+
+    return 0;
+}
