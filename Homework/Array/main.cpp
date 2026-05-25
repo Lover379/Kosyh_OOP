2a4878709b7451ab11322caafe59f8b739922c0e
VEN1KFC <157003153+Lover379@users.noreply.github.com>
2026-05-25 17:32:42 +0800

    Update main.cpp

--- Homework/Array/main.cpp
+++ Homework/Array/main.cpp
@@ -1,5 +1,9 @@
-#include 'Array.h'
-#include iostream
+#include "Array.h"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <chrono>
 
 int main() {
     std::ofstream file("josephus_results.csv");
@@ -8,8 +12,13 @@ int main() {
     std::vector<int> values = { 1000, 5000, 10000, 50000, 100000, 500000, 1000000 };
 
     for (int N : values) {
-        double time = measure_time(N, 2);
+        auto start = std::chrono::high_resolution_clock::now();
+        
         int answer = josephus(N, 2);
+        
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = end - start;
+        double time = duration.count();
 
         file << N << "," << answer << "," << std::fixed << std::setprecision(6) << time << "\n";
 
